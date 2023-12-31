c1ebe09ba91f4ef69e406478cb8fb2e0c1fa161c
Siddhartha Sarkar <mail.04.siddhartha@gmail.com>
2024-01-01 01:32:42 +0530

    main tabs wireframe and styling

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import '../styles/MainContainer.css';
-import { Banner } from './Banner';
+import React, { useEffect } from "react";
+import "../styles/MainContainer.css";
+import { Banner } from "./Banner";
+import { FaUsers } from "react-icons/fa";
 
 const MainContainer = () => {
-  return (
-    <div className='mainContainer'>
-      <Banner  />
-    </div>
-  )
-}
 
-export {MainContainer};
+  useEffect(() => {
+    const allLi = document
+      .querySelector(".menuList")
+      .querySelectorAll("li");
+
+      function changeMenuActive(){
+        allLi.forEach((n) => n.classList.remove("active"));
+        this.classList.add("active");
+      }
+
+      allLi.forEach((n) => n.addEventListener("click", changeMenuActive));
+  }, []);
+
+  return (
+    <div className="mainContainer">
+      <Banner />
+
+      <div className="menuList">
+        <ul>
+          <li>
+            <a href="#">Popular</a>
+          </li>
+          <li>
+            <a href="#">Albums</a>
+          </li>
+          <li>
+            <a href="#">Songs</a>
+          </li>
+          <li>
+            <a href="#">Fans</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+        </ul>
+
+        <p><i><FaUsers /></i>{" "} 20.3M <span>Followers</span></p>
+      </div>
+    </div>
+  );
+};
+
+export { MainContainer };
